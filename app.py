a50165f9f54c7bbfc2ec68691a2519b723bbb8a9
myblipe <myblipe@gmail.com>
2023-02-07 00:34:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     city = 'Jelenia Gora'
     if request.method == 'POST':
         city = request.form['city']
-    api_key = 'dff7b5776a70ad863421e9f2ff5b9543'
+    api_key = 'TUTAJ_NALEŻY_WPISAĆ_KLUCZ_API'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
     weather_data = requests.get(url).json()
     return render_template('index.html', weather_data=weather_data, city=city)
